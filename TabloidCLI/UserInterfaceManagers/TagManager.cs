90e20051c136930e37d0f0dca4c6b36034cd01df
Nathan Center <nathanjcenter@gmail.com>
2022-03-21 15:56:52 -0500

    edit and remove tags

--- TabloidCLI/UserInterfaceManagers/TagManager.cs
+++ TabloidCLI/UserInterfaceManagers/TagManager.cs
@@ -67,6 +67,35 @@ namespace TabloidCLI.UserInterfaceManagers
 
 
         }
+        private Tag Choose(string prompt = null)
+        {
+            if (prompt == null)
+            {
+                prompt = "Please choose an Author:";
+            }
+            Console.WriteLine(prompt);
+            List<Tag> tags = _tagRepository.GetAll();
+
+            for (int i = 0; i < tags.Count; i++)
+            {
+                Tag tag = tags[i];
+                Console.WriteLine($" {i + 1}) {tag.Name}");
+            }
+
+            Console.Write("> ");
+            string input = Console.ReadLine();
+            try
+            {
+                int choice = int.Parse(input);
+                return tags[choice - 1];
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid Selection");
+                return null;
+            }
+
+        }
 
         private void Add()
         {
@@ -75,16 +104,36 @@ namespace TabloidCLI.UserInterfaceManagers
             tag.Name = Console.ReadLine();
             _tagRepository.Insert(tag);
 
+
         }
 
         private void Edit()
         {
-            throw new NotImplementedException();
-        }
+            Tag tagToEdit = Choose("Which tag would you like to edit?");
+            if (tagToEdit == null)
+            {
+                return;
+            }
 
+            Console.WriteLine();
+            Console.Write("New  Name (blank to leave unchanged): ");
+            string Name = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(Name))
+            {
+                tagToEdit.Name = Name;
+            }
+
+            _tagRepository.Update(tagToEdit);
+        }
+      
         private void Remove()
         {
-            throw new NotImplementedException();
+            Tag tagToDelete = Choose("Which tag would you like to remove?");
+            if (tagToDelete != null)
+            {
+                _tagRepository.Delete(tagToDelete.Id);
+            }
+            Console.WriteLine("Tag successfully deleted");
         }
     }
 }
