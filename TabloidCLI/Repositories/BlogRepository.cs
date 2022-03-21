a97f4b4fb1f25bf7454493baf336d99ac34726b1
Isaac Weiser <isaac.weiser.x@gmail.com>
2022-03-21 12:56:59 -0400

    working on blog repo and managaer

---
+++ TabloidCLI/Repositories/BlogRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabloidCLI.Repositories
+{
+    public class BlogRepository
+    {
+        public string Title { set; get; }
+        public string Url { get; set; }
+    }
+}
---
+++ TabloidCLI/UserInterfaceManagers/BlogManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TabloidCLI.Repositories;
+
+namespace TabloidCLI.UserInterfaceManagers
+{
+    internal class BlogManager: BlogRepository, IUserInterfaceManager
+    {
+       
+        public IUserInterfaceManager Execute()
+        {
+            Console.WriteLine("Blog Menu");
+            Console.WriteLine(" 1) Add Blog");
+            Console.WriteLine(" 4) View Blogs");
+            Console.WriteLine(" 5) Remove Blog");
+            Console.WriteLine(" 5) Edit Blog");
+            Console.WriteLine(" 0) Go Back");
+
+            return this;
+        }
+
+    }
+}
