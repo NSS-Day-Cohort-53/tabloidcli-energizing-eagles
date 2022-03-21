dfdfba15574ee9e7eec3d6067ed7ee81b4f75153
Nathan Elmer <nelmer12@gmail.com>
2022-03-21 13:26:38 -0500

    real journal repo

--- TabloidCLI/Repositories/JournalRepository.cs
+++ TabloidCLI/Repositories/JournalRepository.cs
@@ -1,12 +1,92 @@
 ﻿using System;
+using Microsoft.Data.SqlClient;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using TabloidCLI.Models;
 
 namespace TabloidCLI.Repositories
 {
-    internal class JournalRepository
+    public class JournalRepository : DatabaseConnector, IRepository<Journal>
     {
+        public JournalRepository(string connectionString) : base(connectionString) { }
+
+        public List<Journal> GetAll()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Id,
+                                               Title,
+                                               Content,
+                                               CreateDateTime
+                                          FROM Journal";
+
+                    List<Journal> journals = new List<Journal>();
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Journal journal = new Journal()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Title = reader.GetString(reader.GetOrdinal("Title")),
+                            Content = reader.GetString(reader.GetOrdinal("Content")),
+                            CreateDateTime = reader.GetDateTime(reader.GetOrdinal("CreateDateTime")),
+                        };
+                        journals.Add(journal);
+                    }
+
+                    reader.Close();
+
+                    return journals;
+                }
+            }
+        }
+        public Journal Get(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Id,
+                                               Title,
+                                               Content,
+                                               CreateDateTime
+                                        FROM Journal WHERE @id = Id";
+                    cmd.Parameters.AddWithValue("@id", id);
+                    
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        Journal journal = null;
+                        
+                        if (reader.Read())
+                        {
+                            journal = new Journal
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Title = reader.GetString(reader.GetOrdinal("Title")),
+                                Content = reader.GetString(reader.GetOrdinal("Content")),
+                                CreateDateTime = reader.GetDateTime(reader.GetOrdinal("CreateDateTime"))
+                            };
+                        }
+                        return journal;
+                    }
+                }
+            }
+        }
+        public void Insert(Journal entry)
+        {
+
+        }
+        public void Update(Journal entry)
+        {
+
+        }
+        public void Delete(int id)
+        {
+
+        }
     }
 }
